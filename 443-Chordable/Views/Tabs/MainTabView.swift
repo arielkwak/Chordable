57ca2bcd8d1795ca4b56ddc2a2e77d07081fd426
Ariel Kwak <anniekwak123@gmail.com>
2023-10-31 19:54:15 -0400

    Minor changes

--- 443-Chordable/Views/Tabs/MainTabView.swift
+++ 443-Chordable/Views/Tabs/MainTabView.swift
@@ -6,6 +6,12 @@ struct MainTabView: View {
     init() {
       let appearance = UITabBarAppearance()
       appearance.backgroundColor = .black
+      if #available(iOS 14.0, *) {
+           appearance.shadowColor = UIColor(Color(red: 0.14, green: 0, blue: 1))
+       } else {
+           // Fallback on earlier versions
+           appearance.shadowColor = .white
+       }
       UITabBar.appearance().scrollEdgeAppearance = appearance
       UITabBar.appearance().standardAppearance = appearance
     }
@@ -15,24 +21,33 @@ struct MainTabView: View {
             // Chords Tab
             ChordsView(viewController: ChordsViewController())
                 .tabItem {
-                    Image(selectedTab == 0 ? "chords_tab_grey" : "chords_tab_highlighted")
+                    Image(selectedTab == 0 ? "chords_tab_highlighted" : "chords_tab_grey")
                     Text("Chords")
+                    .fontWeight(selectedTab == 0 ? .bold : .regular)
+                    .foregroundColor(selectedTab == 0 ? .white : .gray)
                 }
                 .tag(0)
             
             // Home Tab
             HomeView()
                 .tabItem {
-                    Image(selectedTab == 0 ? "home_tab_grey" : "home_tab_highlighted")
+                    Image(selectedTab == 1 ? "home_tab_highlighted" : "home_tab_grey")
                     Text("Home")
+                    .fontWeight(selectedTab == 1 ? .bold : .regular)
+                    .foregroundColor(selectedTab == 1 ? .white : .gray)
+
                 }
+                .tag(1)
             
             // Songs Tab
             SongsView()
                 .tabItem {
-                    Image(selectedTab == 0 ? "song_tab_grey" : "song_tab_highlighted")
+                    Image(selectedTab == 2 ? "song_tab_highlighted" : "song_tab_grey")
                     Text("Songs")
+                    .fontWeight(selectedTab == 0 ? .bold : .regular)
+                    .foregroundColor(selectedTab == 2 ? .white : .gray)
                 }
+                .tag(2)
         }
     }
 }

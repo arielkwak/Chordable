60dbe7d971cc2ad54cef6cd29e1cef85cc87dc66
Ariel Kwak <anniekwak123@gmail.com>
2023-10-31 13:48:21 -0400

    Tab bar background black - changes still needed

--- 443-Chordable/Views/Tabs/MainTabView.swift
+++ 443-Chordable/Views/Tabs/MainTabView.swift
@@ -1,51 +1,43 @@
-//
-//  TabView.swift
-//  443-Chordable
-//
-//  Created by Owen Gometz on 10/29/23.
-//
-
 import SwiftUI
 
 struct MainTabView: View {
-    
-  @State private var selectedTab: Int = 0  // To keep track of the currently selected tab
+    @State private var selectedTab: Int = 0  // To keep track of the currently selected tab
 
+    init() {
+      let appearance = UITabBarAppearance()
+      appearance.backgroundColor = .black
+      UITabBar.appearance().scrollEdgeAppearance = appearance
+      UITabBar.appearance().standardAppearance = appearance
+    }
 
     var body: some View {
-      TabView {
-          // Home Tab
-          HomeView()
-              .tabItem {
-                  Image(systemName: "house.fill")
-                  Text("Home")
-              }
-          
-          // Chords Tab
-          ChordsView(viewController: ChordsViewController())
-              .tabItem {
-                  Image(systemName: "music.note")
-                  Text("Chords")
-              }
-          
-          // Songs Tab
-          SongsView()
-              .tabItem {
-                  Image(systemName: "music.mic")
-                  Text("Songs")
-              }
+        TabView(selection: $selectedTab) {
+            // Chords Tab
+            ChordsView(viewController: ChordsViewController())
+                .tabItem {
+                    Image(selectedTab == 0 ? "chords_tab_grey" : "chords_tab_highlighted")
+                    Text("Chords")
+                }
+                .tag(0)
+            
+            // Home Tab
+            HomeView()
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            
+            // Songs Tab
+            SongsView()
+                .tabItem {
+                    Image(systemName: "music.mic")
+                    Text("Songs")
+                }
         }
     }
 }
 
 struct MainTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainTabView()
-    }
-}
-
-
-struct TabView_Previews: PreviewProvider {
     static var previews: some View {
         MainTabView()
     }

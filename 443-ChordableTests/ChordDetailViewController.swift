7f5b302c4a5c4ff7630011841c74c4499ee732de
minjooki <minjooki@andrew.cmu.edu>
2023-11-14 11:13:05 -0500

    more tests for timer and mic

--- 443-ChordableTests/ChordDetailViewController.swift
+++ 443-ChordableTests/ChordDetailViewController.swift
@@ -7,19 +7,23 @@
 
 import XCTest
 @testable import _43_Chordable
+import AVFAudio
 
 final class ChordDetailViewControllerTest: XCTestCase {
   
   var chordDetailViewController: ChordDetailViewController!
+  var exampleChord: Chord!
   
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
       super.setUp()
-      chordDetailViewController = ChordDetailViewController()
+      let exampleChord = Chord()
+      chordDetailViewController = ChordDetailViewController(chord: exampleChord)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+      exampleChord = nil
       chordDetailViewController = nil
       super.tearDown()
     }
@@ -28,6 +32,7 @@ final class ChordDetailViewControllerTest: XCTestCase {
     // Play chord with valid chord name
     let goodChordName = "Bm"
     try chordDetailViewController.playChord(chordName: goodChordName)
+    // should be stopped after playing
     XCTAssertEqual(chordDetailViewController.status, .stopped)
     
   }
@@ -43,5 +48,55 @@ final class ChordDetailViewControllerTest: XCTestCase {
       XCTAssertEqual(chordError, ChordDetailError.fileNotFound(chordName: badChordName))
     }
   }
-
+  
+  func testRecorderSetup() {
+    let testRecorder = chordDetailViewController.setupRecorder
+    XCTAssertNoThrow(testRecorder)
+  }
+  
+  func testStartRecording() {
+    XCTAssertNoThrow(chordDetailViewController.startRecording(for: 5) { _ in })
+    XCTAssertEqual(chordDetailViewController.status, .recording)
+  }
+  
+  func testStopRecording() {
+    XCTAssertNoThrow(chordDetailViewController.stopRecording { _ in })
+    XCTAssertEqual(chordDetailViewController.status, .stopped)
+  }
+  
+  func testStartCountdown() {
+    chordDetailViewController.startCountdown()
+    XCTAssertEqual(chordDetailViewController.countdown, 3)
+    XCTAssertTrue(chordDetailViewController.isCountingDown)
+    
+    let expectation = XCTestExpectation(description: "Countdown finished")
+    DispatchQueue.main.asyncAfter(deadline: .now() + 4) { [self] in
+      XCTAssertFalse(chordDetailViewController.isCountingDown)
+      XCTAssertEqual(chordDetailViewController.countdown, 1)
+      expectation.fulfill()
+    }
+    wait(for: [expectation], timeout: 5)
+  }
+  
+  func testStartDuration() {
+    // fails right now --> need to pass in a real Chord to the view controller to start duration
+    chordDetailViewController.startDuration()
+    XCTAssertTrue(chordDetailViewController.isRecordingActive)
+    XCTAssertEqual(chordDetailViewController.duration, 5)
+    
+    let expectation = XCTestExpectation(description: "Recording duration finished")
+    DispatchQueue.main.asyncAfter(deadline: .now() + 6 ) { [self] in
+      XCTAssertFalse(chordDetailViewController.isRecordingActive)
+      XCTAssertEqual(chordDetailViewController.duration, 0)
+      expectation.fulfill()
+    }
+    wait(for: [expectation], timeout: 7)
+  }
+  
+  func testRequestMicrophoneAccess() {
+    XCTAssertFalse(chordDetailViewController.hasMicAccess)
+    chordDetailViewController.requestMicrophoneAccess()
+//    XCTAssertTrue(chordDetailViewController.displayNotification)
+  }
+  
 }

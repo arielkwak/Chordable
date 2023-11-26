f12160c002ae4cf613bb37e169846e7adbb0d337
Ariel Kwak <anniekwak123@gmail.com>
2023-11-25 20:36:08 -0500

    Model for random quotes

--- 443-Chordable/Models/Quote.swift
+++ 443-Chordable/Models/Quote.swift
@@ -8,6 +8,18 @@
 import Foundation
 
 extension Quote {
-    
-  
+    struct Quotes {
+        let quotes: [String: [String]] = [
+            "Adele": ["I don't make music for eyes. I make music for ears."],
+            "Bob Marley": ["One good thing about music, when it hits you, you feel no pain"],
+        ]
+
+        func getRandomQuote() -> (String, String)? {
+            guard let artist = quotes.keys.randomElement(),
+                let quote = quotes[artist]?.randomElement() else {
+                return nil
+            }
+            return (artist, quote)
+        }
+    }
 }

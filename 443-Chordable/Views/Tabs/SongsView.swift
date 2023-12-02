ef0c7db23a3787448740360e9e6b05b3bec30d36
Ariel Kwak <anniekwak123@gmail.com>
2023-12-02 01:23:10 -0500

    reformat existing structure

--- 443-Chordable/Views/Tabs/SongsView.swift
+++ 443-Chordable/Views/Tabs/SongsView.swift
@@ -9,21 +9,19 @@ import SwiftUI
 import CoreData
 
 struct SongsView: View {
-    let genres = ["Pop", "Rock", "R&B / Soul", "Folk & Country", "Alternative"]
-    @Environment(\.managedObjectContext) var context
+  let genres = ["Pop", "Rock", "R&B / Soul", "Folk & Country", "Alternative"]
+  @Environment(\.managedObjectContext) var context
 
-    var body: some View {
-        NavigationView {
-            VStack {
-
-                List(genres, id: \.self) { genre in
-                    NavigationLink(destination: SongsForGenreView(controller: SongsForGenreViewController(context: context, genre: genre))) {
-                        Text(genre)
-                    }
-                }
-                .padding(.top, 30)
-            }
-            .navigationTitle("Songs")
+  var body: some View {
+    NavigationView {
+      VStack {
+        List(genres, id: \.self) { genre in
+          NavigationLink(destination: SongsForGenreView(controller: SongsForGenreViewController(context: context, genre: genre))) {
+              Text(genre)
+          }
         }
+        .padding(.top, 30)
+      }
     }
+  }
 }
